c185ceb0c709f30b28d0e7acd8a3038b19e2ce3a
Lucia <Lucia@192.168.1.37>
2026-04-06 12:59:15 +0100

    MODIFICACION MUESTRA EN PANTALLA

--- src/CalculadoraAplicacion/Calculadora.java
+++ src/CalculadoraAplicacion/Calculadora.java
@@ -5,7 +5,7 @@ public double sumar(double a, double b) { return a + b; }
 public double restar(double a, double b) { return a - b; } 
 public static void main(String[] args) { 
 Calculadora calc = new Calculadora(); 
-System.out.println("--- INICIO DE LA CALCULADORA ---"); 
+System.out.println("--- CALCULADORA DEL JEFE A ---"); 
 System.out.println("Suma: " + calc.sumar(10, 5)); 
 } 
 }
